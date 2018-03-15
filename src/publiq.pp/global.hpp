7b0bc8341a6a88e395136041f2278e41333c0da9
Tigran Ghahramanyan <tghahram@gmail.com>
2018-03-15 17:18:48 +0400

    core structure

---
+++ src/publiq.pp/global.hpp
@@ -0,0 +1,12 @@
+#include <belt.pp/global.hpp>
+
+#define PUBLIQ_EXPORT BELT_EXPORT
+#define PUBLIQ_IMPORT BELT_IMPORT
+
+#ifdef B_OS_UNIX
+#define P_OS_UNIX
+#endif
+
+#ifdef B_OS_WINDOWS
+#define P_OS_WINDOWS
+#endif
