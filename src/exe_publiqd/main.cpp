e80b6de0630588f21fe3ac9a34e55b411cac1f86
Tigran Ghahramanyan <tghahram@gmail.com>
2018-04-13 18:24:05 +0400

    we can pass Hellow message through lower level p2psocket protocols

--- src/exe_publiqd/main.cpp
+++ src/exe_publiqd/main.cpp
@@ -1,6 +1,7 @@
 #include "message.hpp"
 
 #include <belt.pp/message_global.hpp>
+#include <belt.pp/log.hpp>
 
 #include <mesh.pp/p2psocket.hpp>
 
@@ -10,8 +11,13 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <chrono>
 
 using namespace PubliqNodeMessage;
+
+using peer_id = meshpp::p2psocket::peer_id;
+using packet = meshpp::p2psocket::packet;
+using packets = meshpp::p2psocket::packets;
 
 namespace program_options = boost::program_options;
 
@@ -20,6 +26,8 @@ using std::string;
 using std::cout;
 using std::endl;
 using std::vector;
+namespace chrono = std::chrono;
+using chrono::steady_clock;
 
 using sf = meshpp::p2psocket_family_t<
     Error::rtt,
@@ -38,7 +46,122 @@ using sf = meshpp::p2psocket_family_t<
 
 bool process_command_line(int argc, char** argv,
                           beltpp::ip_address& bind_to_address,
-                          vector<beltpp::ip_address>& connect_to_addresses)
+                          vector<beltpp::ip_address>& connect_to_addresses,
+                          string& greeting);
+
+inline string short_name(string const& peerid)
+{
+    return peerid.substr(0, 5);
+}
+
+int main(int argc, char** argv)
+{
+    beltpp::ip_address bind_to_address;
+    vector<beltpp::ip_address> connect_to_addresses;
+    string greeting;
+
+    if (false == process_command_line(argc, argv,
+                                      bind_to_address,
+                                      connect_to_addresses,
+                                      greeting))
+        return 1;
+
+    try
+    {
+        cout << bind_to_address.to_string() << endl;
+        for (auto const& item : connect_to_addresses)
+            cout << item.to_string() << endl;
+
+
+        beltpp::message_loader_utility utl;
+        PubliqNodeMessage::detail::extension_helper(utl);
+        auto ptr_utl =
+                beltpp::new_void_unique_ptr<beltpp::message_loader_utility>(&utl);
+
+        beltpp::ilog_ptr plogger = beltpp::console_logger("exe_publiqd");
+        plogger->disable();
+
+        meshpp::p2psocket sk = meshpp::getp2psocket<sf>(bind_to_address,
+                                                        connect_to_addresses,
+                                                        std::move(ptr_utl),
+                                                        plogger.get());
+        sk.set_timer(chrono::seconds(10));
+
+        cout << endl;
+        cout << "Node: " << short_name(sk.name()) << endl;
+        cout << endl;
+
+        while (true)
+        {
+            try
+            {
+                meshpp::p2psocket::peer_id peerid;
+
+                packets received_packets = sk.receive(peerid);
+
+                for (auto const& received_packet : received_packets)
+                {
+                    switch (received_packet.type())
+                    {
+                    case Join::rtt:
+                    {
+                        cout << short_name(peerid) << " joined" << endl;
+
+                        if (greeting.empty() == false)
+                        {
+                            cout << "sending Hellow to: "
+                                 << short_name(peerid)
+                                 << endl;
+
+                            Hellow msg_send;
+                            msg_send.text = greeting;
+                            sk.send(peerid, msg_send);
+                        }
+                        break;
+                    }
+                    case Drop::rtt:
+                        cout << short_name(peerid) << " dropped" << endl;
+                        break;
+                    case TimerOut::rtt:
+                        cout << "timer" << endl;
+                        break;
+                    case Hellow::rtt:
+                    {
+                        Hellow hellow_msg;
+                        received_packet.get(hellow_msg);
+
+                        cout << "Hellow: " << hellow_msg.text << endl;
+                        cout << "From: " << short_name(peerid) << endl;
+                        break;
+                    }
+                    }
+                }
+            }
+            catch (std::exception const& ex)
+            {
+                cout << "exception cought: " << ex.what() << endl;
+            }
+            catch (...)
+            {
+                cout << "always throw std::exceptions" << endl;
+            }
+        }
+    }
+    catch (std::exception const& ex)
+    {
+        cout << "exception cought: " << ex.what() << endl;
+    }
+    catch (...)
+    {
+        cout << "always throw std::exceptions" << endl;
+    }
+    return 0;
+}
+
+bool process_command_line(int argc, char** argv,
+                          beltpp::ip_address& bind_to_address,
+                          vector<beltpp::ip_address>& connect_to_addresses,
+                          string& greeting)
 {
     string interface;
     vector<string> hosts;
@@ -50,7 +173,9 @@ bool process_command_line(int argc, char** argv,
             ("interface,i", program_options::value<string>(&interface)->required(),
                             "The local network interface and port to bind to")
             ("connect,c", program_options::value<vector<string>>(&hosts),
-                            "Remote nodes addresss with port");
+                            "Remote nodes addresss with port")
+            ("greeting,g", program_options::value<string>(&greeting),
+                            "send a greeting message to all peers");
         (void)(desc_init);
 
         program_options::variables_map options;
@@ -92,42 +217,4 @@ bool process_command_line(int argc, char** argv,
     }
 
     return true;
-}
-
-int main(int argc, char** argv)
-{
-    beltpp::ip_address bind_to_address;
-    vector<beltpp::ip_address> connect_to_addresses;
-
-    if (false == process_command_line(argc, argv,
-                                      bind_to_address,
-                                      connect_to_addresses))
-        return 1;
-
-    try
-    {
-        cout << bind_to_address.to_string() << endl;
-        for (auto const& item : connect_to_addresses)
-            cout << item.to_string() << endl;
-
-
-        beltpp::message_loader_utility utl;
-        PubliqNodeMessage::detail::extension_helper(utl);
-        auto ptr_utl =
-                beltpp::new_void_unique_ptr<beltpp::message_loader_utility>(&utl);
-
-        meshpp::p2psocket sk = meshpp::getp2psocket<sf>(bind_to_address,
-                                                        connect_to_addresses,
-                                                        std::move(ptr_utl),
-                                                        nullptr);
-    }
-    catch (std::exception const& ex)
-    {
-        cout << "exception cought: " << ex.what() << endl;
-    }
-    catch (...)
-    {
-        cout << "always throw std::exceptions" << endl;
-    }
-    return 0;
 }
