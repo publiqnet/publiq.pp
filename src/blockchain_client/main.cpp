dc0e4d6a39eb62466f2131298e72d3b640c59e70
Gor Mkrtchyan <gormkrtchyan.g@gmail.com>
2018-07-31 10:49:28 +0400

    fix up send-receive process

--- src/blockchain_client/main.cpp
+++ src/blockchain_client/main.cpp
@@ -1,4 +1,4 @@
-#include "../libblockchain/message.hpp"
+﻿#include "../libblockchain/message.hpp"
 
 #include <belt.pp/log.hpp>
 #include <belt.pp/packet.hpp>
@@ -15,8 +15,10 @@
 #include <memory>
 #include <exception>
 
+#include <unistd.h> //for sleep() function
 
 using namespace BlockchainMessage;
+using pr_id = beltpp::socket::peer_id;
 //namespace program_options = boost::program_options;
 
 using std::unique_ptr;
@@ -28,10 +30,10 @@ namespace chrono = std::chrono;
 using chrono::steady_clock;
 using std::runtime_error;
 
-bool process_command_line(int argc, char** argv);
-
-using namespace BlockchainMessage;
-
+beltpp::socket openChannel(char** argv, pr_id& channel_id, beltpp::event_handler& evH);
+template<typename T>
+void SendReceive(T &evType, beltpp::socket& sk, pr_id channel_id,
+                                                beltpp::event_handler& evH);
 
 //  MSVS does not instansiate template function only because its address
 //  is needed, so let's force it
@@ -54,65 +56,174 @@ using sf = beltpp::socket_family_t<
 
 int main(int argc, char** argv)
 {
-    const int listen_count = 10;
-
+    pr_id channel_id;
     beltpp::event_handler eh;
-    beltpp::socket sk = beltpp::getsocket<sf>(eh);
-    eh.add(sk);
+    beltpp::socket sk = openChannel(argv, channel_id, eh);
 
-    std::vector<beltpp::socket::peer_id> arr_channel_id;
-    arr_channel_id.resize(100);
+    cout << channel_id << endl;
 
-    for (size_t i = 0; i < 20000; ++i)
-    {
-        short index = short(i % arr_channel_id.size());
+    //
+    LoggedTransactionsRequest logTransR;
+    logTransR.start_index = 5;
+    //
+    ChainInfoRequest chainIR;
+    //
+    KeyPairRequest keyPR;
+    keyPR.index = 2;
+    keyPR.master_key = "master_key = 000001";
+    //
+    SignRequest signR;
+    signR.private_key = "5JNncPzzupdnuCtXT1y91WJoUhevSF6GSinPUmdb8L2fNVdrkig";
+    //
+    DigestRequest digestR;
+    //
+    /*
+    MasterKeyRequest masterKeyR;
+    //
+    SyncRequest syncReq;
+    syncReq.block_number = 5;
+    syncReq.consensus_sum = 5;
+    //
+    BlockChainRequest blockChainReq;
+    blockChainReq.blocks_from = 0;
+    blockChainReq.blocks_to = 5;
+    */
 
-        beltpp::ip_address open_address("", 0, "127.0.0.1", 3550 + (i % listen_count), beltpp::ip_address::e_type::ipv4);
-        sk.open(open_address);
-
-        std::unordered_set<beltpp::ievent_item const*> set_items;
-        while (true)
-        {
-            beltpp::socket::peer_id channel_id;
-
-            beltpp::isocket::packets pcs;
-            if (beltpp::ievent_handler::wait_result::event == eh.wait(set_items))
-                pcs = sk.receive(channel_id);
-
-            if (channel_id.empty())
-                continue;
-
-            for (auto const& pc : pcs)
-            {
-                if (pc.type() == Join::rtt)
-                {
-                    std::cout << i << std::endl;
-                    if (i >= arr_channel_id.size())
-                        sk.send(arr_channel_id[index], Drop());
-
-                    arr_channel_id[index] = channel_id;
-                }
-            }
-            break;
-        }
-
-        //std::cout << sk.dump() << std::endl;
-    }
-
-
-    if (false == process_command_line(argc, argv))
-        return 1;
+    SendReceive(logTransR, sk, channel_id, eh);
+    SendReceive(logTransR, sk, channel_id, eh);
+    SendReceive(logTransR, sk, channel_id, eh);
+    SendReceive(chainIR, sk, channel_id, eh);
+    SendReceive(keyPR, sk, channel_id, eh);
+    SendReceive(keyPR, sk, channel_id, eh);
+    SendReceive(keyPR, sk, channel_id, eh);
+    SendReceive(digestR, sk, channel_id, eh);
+    SendReceive(digestR, sk, channel_id, eh);
+    SendReceive(signR, sk, channel_id, eh);
+    Shutdown shutDown;
+    SendReceive(shutDown, sk, channel_id, eh);
 
     return 0;
 }
 
-bool process_command_line(int argc, char** argv)
+beltpp::socket openChannel(char** argv, pr_id& channel_id, beltpp::event_handler& eh)
 {
+    beltpp::socket sk = beltpp::getsocket<sf>(eh);
+    eh.add(sk);
 
-    cout << "The command line args count = " << argc << endl;
+    beltpp::ip_address address_item;
+    address_item.from_string(argv[1]);
+    beltpp::ip_address open_address("", 0, address_item.local.address,
+                                                       address_item.local.port,
+                                                       beltpp::ip_address::e_type::ipv4);
+    sk.open(open_address);
 
-    for(int i = 1; i<argc; ++i)
-        cout << argv[i] << endl;
+    beltpp::isocket::packets pcs;
+    std::unordered_set<beltpp::ievent_item const*> set_items;
+    while(true)
+    {
+        if (beltpp::ievent_handler::wait_result::event == eh.wait(set_items))
+            pcs = sk.receive(channel_id);
+        if (channel_id.empty() && pcs.empty())
+            continue;
+        else
+            break;
+    }
 
-    return true;
+    return sk;
 }
+
+template<typename T>
+void SendReceive(T &evType, beltpp::socket& sk, pr_id channel_id,
+                                                    beltpp::event_handler& eh)
+{
+    sk.send(channel_id, evType);
+
+    bool isContinue{true};
+    while(isContinue)
+    {
+        beltpp::isocket::packets pcs;
+        std::unordered_set<beltpp::ievent_item const*> set_items;
+        if (beltpp::ievent_handler::wait_result::event == eh.wait(set_items))
+            pcs = sk.receive(channel_id);
+        if (channel_id.empty() || pcs.empty())
+            continue;
+        for(auto &pc:pcs)
+        {
+            switch(pc.type())
+            {
+            case Join::rtt:
+                std::cout << "The event type is Join: " << endl << endl;
+                isContinue = false;
+                break;
+            case LoggedTransactions::rtt:
+            {
+                LoggedTransactions logTrans;
+                pc.get(logTrans);
+
+                auto index = 0;
+                if(logTrans.actions.size() > 0)
+                   index = logTrans.actions[0].index;
+                cout << "The start_index is = " << index << endl << endl;
+                isContinue = false;
+                break;
+            }
+            case ChainInfo::rtt:
+            {
+                ChainInfo chainInfo;
+                pc.get(chainInfo);
+                cout << "The ChainInfo length = " << chainInfo.length << endl << endl;
+                isContinue = false;
+                break;
+            }
+            case KeyPair::rtt:
+            {
+                KeyPair keyP;
+                pc.get(keyP);
+                cout << "The KeyPair index = " << keyP.index << endl;
+                cout << "The KeyPair public_key = " << keyP.public_key << endl;
+                cout << "The KeyPair private_key = " << keyP.private_key << endl;
+                cout << "The KeyPair master_key = " << keyP.master_key << endl << endl;
+                isContinue = false;
+                break;
+            }
+            case Digest::rtt:
+            {
+                Digest digest;
+                pc.get(digest);
+                cout << "The Digest base58_hash = " << digest.base58_hash << endl << endl;
+                isContinue = false;
+                break;
+            }
+            case Signature::rtt:
+            {
+                Signature sign;
+                pc.get(sign);
+                cout << "The Signature public_key = " << sign.public_key << endl;
+                cout << "The Signature signature = " << sign.signature << endl << endl;
+                isContinue = false;
+                break;
+            }
+            case RemoteError::rtt:
+                //RemoteError error;
+                //pc.get(error);
+                //cout << "RemoteError: " << rError.message <<endl;
+                cout << "Received a RemoteError!!!" << endl << endl;
+                isContinue = false;
+                break;
+            case Drop::rtt:
+                cout << "The process was Dropped!" << endl << endl;
+                isContinue = false;
+                break;
+            case Done::rtt:
+                cout << "The Send-Receive process was Shutted Down!" << endl << endl;
+                isContinue = false;
+                break;
+            default:
+                cout << "Nothing for receive.!" << endl;
+                isContinue = false;
+                break;
+            }
+        }
+    }
+}
+
