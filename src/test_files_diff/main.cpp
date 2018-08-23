b19e25c6a710ca84210a2e8d7d50bcd88823a606
SonaBadalyan <sona.badalyan.1992@gmail.com>
2018-08-23 10:09:04 +0400

    fix

--- src/test_files_diff/main.cpp
+++ src/test_files_diff/main.cpp
@@ -22,12 +22,12 @@ string splitLast(const string& path)
 
 void compare(const string& path1, const string&  path2)
 {
-//    ifstream first(path1);
-//    ifstream second(path2);
-//
+    ifstream first(path1);
+    ifstream second(path2);
+
     fs::path bfirst(path1);
     fs::path bsecond(path2);
-<<<<<<< HEAD
+
 
     if (!fs::exists(bfirst))
     {
@@ -85,104 +85,7 @@ void compare(const string& path1, const string&  path2)
     }
 }
 
-int main(int argc, char* argv[])
-=======
-//
-//    if (!fs::exists(bfirst))
-//    {
-//        cout<<bfirst<<" incorrect path!"<<endl;
-//        return;
-//    }
-//
-//    if (!fs::exists(bsecond))
-//    {
-//        cout<<bsecond<<" incorrect path!"<<endl;
-//        return;
-//    }
-//
-//    if (!fs::is_directory(bfirst) && fs::is_directory(bsecond))
-//    {
-//        cout<<"compare file with directory!"<<endl;
-//        return;
-//    }
-//
-//    if (fs::is_directory(bfirst) && !fs::is_directory(bsecond))
-//    {
-//        cout<<"compare directory with file!"<<endl;
-//        return;
-//    }
-//
-//    if (!fs::is_directory(bfirst) && !fs::is_directory(bsecond))
-//    {
-//
-//        for (string s; getline(first, s); )
-//        {
-//            for (string h; getline(second,h); )
-//            {
-//                if (s.compare(h))
-//                {
-//                    cout<<"file "<<path1<<" mismatch with file "<<path2<<endl;
-//                    //cout << h << "\n " << s << endl;
-//                 }
-//             }
-//        }
-//    }
-//
-//    if (fs::is_directory(bfirst) && fs::is_directory(bsecond))
-//    {
-//
-//            if (fs::is_empty(bfirst))
-//            {
-//                cout<<bfirst<<" is empty!"<<endl;
-//                return;
-//            }
-//            if (fs::is_empty(bsecond))
-//            {
-//                cout<<bsecond<<" is empty!"<<endl;
-//                return;
-//            }
-//
-//            boost::filesystem::directory_iterator end_itr;
-//            boost::filesystem::directory_iterator itrf(bfirst);
-//            boost::filesystem::directory_iterator itrs(bsecond);
-//
-//
-//            for ( ;  itrf != end_itr && itrs != end_itr; ++itrf, ++itrs)
-//            {
-//
-////                   if (is_regular_file(itrf->path()) && is_regular_file(itrs->path())) {
-//
-////                        cout<<itrf->path().string()<<endl;
-////                        cout<<itrs->path().string()<<endl;
-////                        compare(itrf->path().string(), itrs->path().string());
-////                   }
-//
-////                   if ( is_directory(itrf->path()) && is_directory(itrs->path())) {
-//
-////                       cout<<itrf->path().string()<<endl;
-////                       cout<<itrs->path().string()<<endl;
-//
-//                    if ( splitLast(itrf->path().string()) == splitLast(itrs->path().string()) )
-//                    {
-//                        compare(itrf->path().string(), itrs->path().string());
-//                    }
-//
-////                   }
-////                   else {
-////                       if (itrf->path()) {
-////                           cout<<"sdf"<<endl;
-////                       }
-////                       if (itrs->path()) {
-////                           cout<<"fsdfs"<<endl;
-////                       }
-////                   }
-//
-//            }
-//    }
-}
-
 int main(int argc, char* argv[]) 
->>>>>>> d28ce9f822f6f1f9b18b54720bc4de566bc12a83
 {
 
     if (argc>2)
