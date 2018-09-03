0aadc30837f1aedbe9b13991c51ba4061d128847
tigran <tigran@blockchainrndhub.com>
2018-09-03 14:43:14 +0400

    parser performance improvement 3x

--- src/test_loader_simulation/main.cpp
+++ src/test_loader_simulation/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <chrono>
 #include <string>
+#include <fstream>
 
 using namespace BlockchainMessage;
 
@@ -16,62 +17,24 @@ namespace chrono = std::chrono;
 using std::chrono::steady_clock;
 using std::string;
 
-inline
-beltpp::void_unique_ptr get_putl()
-{
-    beltpp::message_loader_utility utl;
-    BlockchainMessage::detail::extension_helper(utl);
-
-    auto ptr_utl =
-        beltpp::new_void_unique_ptr<beltpp::message_loader_utility>(std::move(utl));
-
-    return ptr_utl;
-}
-
 int main(int argc, char** argv)
 {
-    try
+    std::ifstream fl("/Users/tigran/blockchain.9771");
+    std::istreambuf_iterator<char> begin(fl), end;
+    string str(begin, end);
+    SignedBlock sb;
+
+    auto const count = 1000;
+    steady_clock::time_point start = steady_clock::now();
+
+    for (size_t index = 0; index < count; ++index)
     {
-        if (argc < 4)
-        {
-            cout << "usage: test_loader_simulation folder index_from index_to" << endl;
-            return 0;
-        }
-
-        string path = argv[1];
-        size_t pos;
-        size_t index_from = size_t(beltpp::stoi64(argv[2], pos));
-        size_t index_to = size_t(beltpp::stoi64(argv[3], pos));
-
-        cout << "path: " << path << endl;
-        cout << "index from: " << index_from << endl;
-        cout << "index to: " << index_to << endl;
-
-        auto tp_start = steady_clock::now();
-
-        meshpp::vector_loader<Coin> actions("actions", path, 2, get_putl());
-
-        Coin a;
-
-        for (size_t index = 0; index < 100; ++index)
-        {
-            a.fraction = index;
-            actions.push_back(a);
-        }
-
-        cout << a.to_string() << endl;
-
-        actions.save();
-        actions.commit();
-
-        auto duration = steady_clock::now() - tp_start;
-        auto  ms_duration = chrono::duration_cast<chrono::milliseconds>(duration);
-        cout << ms_duration.count() << " ms" << endl;
+        sb.from_string(str);
     }
-    catch(std::exception const& e)
-    {
-        cout << "exception: " << e.what() << endl;
-    }
+    chrono::milliseconds duration =
+            chrono::duration_cast<chrono::milliseconds>(steady_clock::now() - start);
+
+    cout << duration.count() << " milliseconds" << endl;
 
     return 0;
 }
