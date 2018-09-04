b3e55d1b072d6c6d91fa1bdcc1c1e1fce57d3f2b
SonaBadalyan <sona.badalyan.1992@gmail.com>
2018-09-04 17:20:35 +0400

    idl_TypeScript generate correct code

--- src/test_actionlog_diff/main.cpp
+++ src/test_actionlog_diff/main.cpp
@@ -41,6 +41,8 @@ int main( int argc, char** argv )
             return 0;
         }
 
+    std::cout << "Process running ..." << std::endl;
+
     beltpp::ip_address address1;
     address1.from_string( argv[1] );
     if ( address1.remote.empty() )
@@ -55,45 +57,67 @@ int main( int argc, char** argv )
     peerid1 = Connect( address1, sk1, eh1 );
     beltpp::packet receive_package1;
 
+
+    beltpp::ip_address address2;
+    address2.from_string( argv[2] );
+    if ( address2.remote.empty() )
+    {
+        address2.remote = address2.local;
+        address2.local = beltpp::ip_destination();
+    }
+    beltpp::socket::peer_id peerid2;
+    beltpp::event_handler eh2;
+    beltpp::socket sk2 = beltpp::getsocket<sf>( eh2 );
+    eh2.add( sk2 );
+    peerid2 = Connect( address2, sk2, eh2 );
+    beltpp::packet receive_package2;
+
     LoggedTransactionsRequest logged_transactions_request1;
-    logged_transactions_request1.start_index = std::atoll( argv[3] );
+    logged_transactions_request1.start_index = 1;
     Send( logged_transactions_request1, receive_package1, sk1, peerid1, eh1 );
     LoggedTransactions logged_transactions1;
     receive_package1.get( logged_transactions1 );
 
+    LoggedTransactionsRequest logged_transactions_request2;
+    logged_transactions_request2.start_index = 1;
+    Send( logged_transactions_request2, receive_package2, sk2, peerid2, eh2 );
+    LoggedTransactions logged_transactions2;
+    receive_package2.get( logged_transactions2 );
 
+    auto it1 = logged_transactions1.actions.begin();
+    auto it2 = logged_transactions2.actions.begin();
+    for ( ; it2 != logged_transactions2.actions.end() && it1 != logged_transactions1.actions.end(); it1++, it2++ )
+    {
+//        if ( *it1 != *it2 )
+//        {
+//         std::cout << "not smae" << std::endl;
+//         return 0;
+//        }
+        if ( it1->applied_reverted != it2->applied_reverted )
+        {
+            std::cout << "applied_reverted" << std::endl;
+            return 0;
+        }
+        if ( it1->index != it2->index )
+        {
+            std::cout << "index" << std::endl;
+            return 0;
+        }
+        if ( it1->action.to_string() != it2->action.to_string() )
+        {
+            std::cout << "action.to_string()" << std::endl;
+            return 0;
+        }
 
-//    beltpp::ip_address address2;
-//    address2.from_string( argv[2] );
-//    if ( address2.remote.empty() )
-//    {
-//        address2.remote = address2.local;
-//        address2.local = beltpp::ip_destination();
-//    }
-//    beltpp::socket::peer_id peerid2;
-//    beltpp::event_handler eh2;
-//    beltpp::socket sk2 = beltpp::getsocket<sf>( eh2 );
-//    eh2.add( sk2 );
-//    peerid2 = Connect( address2, sk2, eh2 );
-//    beltpp::packet receive_package2;
+    }
 
-//    LoggedTransactionsRequest logged_transactions_request2;
-//    logged_transactions_request2.start_index = std::atoll( argv[3] );
-//    Send( logged_transactions_request2, receive_package2, sk2, peerid2, eh2 );
-//    LoggedTransactions logged_transactions2;
-//    receive_package2.get( logged_transactions2 );
+    if ( it1 != logged_transactions1.actions.end() || it2 != logged_transactions2.actions.end() )
+    {
+        std::cout << " Different action count " << std::endl;
+        return 0;
+    }
 
-//    auto it1 = logged_transactions1.actions.begin();
-//    auto it2 = logged_transactions2.actions.begin();
-//    for ( ; it2 != logged_transactions2.actions.end() && it1 != logged_transactions1.actions.end(); it1++, it2++ )
-//    {
-
-//        if ( it1->applied_reverted != it2->applied_reverted ) return 0;
-//        if ( it1->index != it2->index ) return 0;
-//        if ( it1->action != it2->action ) return 0;
-//    }
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Congrats! action logs are same" << std::endl;
 
     }
     catch ( std::exception const& e )
@@ -110,10 +134,8 @@ void Send(beltpp::packet&& send_package,
           peer_id peerid,
           beltpp::event_handler& eh)
 {
-   cout << endl << endl << "Package sent -> "<< endl << send_package.to_string() << endl;
-
+   cout << endl << "Request -> " << send_package.to_string() << endl;
    sk.send(peerid, std::move(send_package));
-
    while (true)
    {
        beltpp::isocket::packets packets;
@@ -153,8 +175,7 @@ void Send(beltpp::packet&& send_package,
 
        break;
    }
-
-   cout << endl << "Package received -> " << endl << receive_package.to_string() << endl;
+   cout << endl << "Response <- " << endl << receive_package.to_string() << endl;
 }
 
 peer_id Connect(beltpp::ip_address const& open_address,
