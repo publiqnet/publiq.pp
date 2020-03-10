62f436d734a2c90336fb287ae0ca091ee2396de4
Armen Kocharyan <arm.kocharyan@gmail.com>
2020-03-10 14:10:25 +0400

    commander storage management optimization

--- src/commander/program_options.hpp
+++ src/commander/program_options.hpp
@@ -7,5 +7,6 @@
 bool process_command_line(int argc, char** argv,
                           std::string& prefix,
                           std::string& str_pv_key,
+                          uint64_t& sync_interval,
                           beltpp::ip_address& connect_to_address,
                           beltpp::ip_address& listen_on_address);
