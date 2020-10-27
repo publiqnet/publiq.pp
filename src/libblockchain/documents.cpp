b2fb8d6bbb55e163a1acfd08e742bb4fb4f3eec8
Tigran Ghahramanyan <tghahram@gmail.com>
2020-10-28 03:58:58 +0400

    try to fix sponsoring bug

--- src/libblockchain/documents.cpp
+++ src/libblockchain/documents.cpp
@@ -527,6 +527,10 @@ map<string, map<string, coin>> documents::sponsored_content_unit_set_used(publiq
         end_tp = chrono::time_point_cast<chrono::seconds>(end_tp);
         auto start_tp = system_clock::from_time_t(cusi.time_points_used.back().tm);
 
+        coin part_sum;
+        if (sponsored_content_unit_set_used_apply == type && end_tp <= start_tp)
+            throw std::logic_error("sponsored_content_unit_set_used_apply == type && end_tp <= start_tp");
+
         if ((sponsored_content_unit_set_used_apply == type && end_tp > start_tp) ||
             (sponsored_content_unit_set_used_revert == type &&
              (
@@ -590,9 +594,10 @@ map<string, map<string, coin>> documents::sponsored_content_unit_set_used(publiq
                 if (part_end_tp == item_end_tp)
                     part += whole % uint64_t(chrono::duration_cast<chrono::seconds>(whole_duration).count());
 
-                assert(part != coin());
                 if (part == coin())
-                    throw std::logic_error("part == coin()");
+                    continue;
+
+                part_sum += part;
 
                 auto& temp_result = result[item.sponsor_address];
 
@@ -667,7 +672,8 @@ map<string, map<string, coin>> documents::sponsored_content_unit_set_used(publiq
         }
 
         if (sponsored_content_unit_set_used_apply == type &&
-            transaction_hash_to_cancel.empty())
+            transaction_hash_to_cancel.empty() &&
+            part_sum != coin())
         {
             //  pretend mode cannot enter this path
             //
